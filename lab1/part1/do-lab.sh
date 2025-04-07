b9057acbad27bc9a80f02e3c59b49f9dcbff65d5
William Antony <william@antony.com>
2025-04-07 03:58:34 +0000

    Solution 1

--- lab1/part1/do-lab.sh
+++ lab1/part1/do-lab.sh
@@ -3,3 +3,9 @@
 # INCLUDE ALL COMMANDS NEEDED TO PERFORM THE LAB
 # This file will get called from capture_submission.sh
 
+sw ip link add name mybridge type bridge
+sw ip link set mybridge up
+sw ip link set eth1 master mybridge
+sw ip link set eth2 master mybridge
+sw ip link set eth3 master mybridge
+sw ip link set eth3 master mybridge
