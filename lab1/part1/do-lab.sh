a4b3d7eada3776bf7f57dafa9f6bc921752f2056
William Antony <william@antony.com>
2025-04-07 04:07:52 +0000

    Solution 2

--- lab1/part1/do-lab.sh
+++ lab1/part1/do-lab.sh
@@ -3,9 +3,9 @@
 # INCLUDE ALL COMMANDS NEEDED TO PERFORM THE LAB
 # This file will get called from capture_submission.sh
 
-sw ip link add name mybridge type bridge
-sw ip link set mybridge up
-sw ip link set eth1 master mybridge
-sw ip link set eth2 master mybridge
-sw ip link set eth3 master mybridge
-sw ip link set eth3 master mybridge
+docker exec -it clab-lab1-part1-switch ip link add name mybridge type bridge
+docker exec -it clab-lab1-part1-switch ip link set mybridge up
+docker exec -it clab-lab1-part1-switch ip link set eth1 master mybridge
+docker exec -it clab-lab1-part1-switch ip link set eth2 master mybridge
+docker exec -it clab-lab1-part1-switch ip link set eth3 master mybridge
+docker exec -it clab-lab1-part1-switch ip link set eth3 master mybridge
